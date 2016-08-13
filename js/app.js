47eb095fce3a287f766e4a68a4bb75b10c4c6a74
Mark Williams <github@shymedia.com>
2016-08-12 21:52:28 -0700

    1. Added appropriate acknowledements and disclaimers. 2. Added tooltip effect to the intro pages. 3. Fixed line spacing issues on the question text. 4. Tried to fix all aria issues, but failed. Cannot get text announced. I'll try to get help next week.

--- js/app.js
+++ js/app.js
@@ -51,7 +51,6 @@ var QuestionModel = function(options) {
   
 	this.validate = function(value) {
 		this.guessed_id = value;
-		console.log("QuestionModel:: validate: " + value);
 		return this.is_correct();
 	}
   
@@ -83,7 +82,6 @@ var AnswerModel = function(options) {
 
 
 var QuizModerator = function(options) {
-	console.log("Creating quizmoderator");
 	this.introduction = markdown_to_html(options.introduction) || "Oops, we are missing an introduction";
 	this.final_text = markdown_to_html(options.final_text) || "Here should be some feedback.";
 	this.questions = parse_questions(options);
@@ -138,14 +136,12 @@ var QuizController = function(options) {
 	}	
 	
 	this.refreshUI = function() {
-		console.log("!Refreshing UI");	
 		if (this.qmod.quiz_complete()) {
 			this.show_final_score();
 			return;
 		} 
 		if ($("#quiz").hasClass('hidden')) {
-			$("#quiz").removeAttr('hidden');
-			$("#quiz").removeClass('hidden');
+			$("#quiz").removeAttr('hidden').removeClass('hidden');
 		}
 		this.refresh_question_text();
 		this.refresh_answers();
@@ -174,7 +170,6 @@ var QuizController = function(options) {
 	}
 	
 	this.refresh_answers = function() {
-		console.log("Refreshing answers");
 		var q = this.qmod.current_q();
 		$("#answer-list").find(".answer").remove();
 		jQuery.each(q.answers, function(key, value) {
@@ -200,7 +195,6 @@ var QuizController = function(options) {
 			$("#answer-list").append(new_answer);
 		})
 		this.watch_answers_for_change();
-		console.log("Finished refreshing answers.");
 	}
 	
 	this.answer_selected = function() {
@@ -222,9 +216,7 @@ var QuizController = function(options) {
 				the_button.text("Continue");
 				the_button.attr('title', "Continue to the next question or to see your results.");
 				$("#quiz").off("submit");
-				console.log("Setting form to continue");
 				 $("#quiz").submit(function( event ) {
-				 	console.log("continue");
 				 	event.preventDefault();
   					quiz_controller.continue_quiz();
 				})
@@ -248,7 +240,6 @@ var QuizController = function(options) {
 				the_button.attr('title', button_title);
 				$("#quiz").off("submit");
 				$("#quiz").submit(function( event ) {
-					console.log("Submitting, should be checking...");
 					event.preventDefault();
   					quiz_controller.verify_answer(event);
 				})
@@ -280,16 +271,13 @@ var QuizController = function(options) {
 		if (this.qmod.current_q().completed()) {
 			this.qmod.next_question();
 			this.refreshUI();
-		} else {
-			console.log("Current question is not complete!");
-		}
+		} 
 	}
 	
 	this.show_final_score = function() {
-		$("#quiz").attr('hidden');
+		$("#quiz").attr('hidden')
 		$("#quiz").addClass('hidden');
-		$("#completed-quiz").removeAttr('hidden');
-		$("#completed-quiz").removeClass('hidden');
+		$("#completed-quiz").removeAttr('hidden').removeClass('hidden');
 		var score_percentage = 0;
 		score_percentage = (this.qmod.total_score()/this.qmod.total_questions()) * 100;
 		var fire_fireworks = false;
@@ -342,7 +330,7 @@ var QuizController = function(options) {
 
 }
 
-var q1Data = '{ "introduction": "This quiz is about the song _Is That All There Is?_", "final_text": "Read more on Wikipedia\x27s entry on [Is that all there is?](https://en.wikipedia.org/wiki/Is_That_All_There_Is%3F)", "questions": [ { "id": "12", "question_visual_id": "1", "question_text": "Select a quiz type.", "answer_ids": [ "123" ], "answers": [ { "id": "121", "numerator": "a", "accesskey": "a", "text": "Hard quiz" }, { "id": "122", "numerator": "b", "accesskey": "b", "text": "Easy quiz" }, { "id": "123", "numerator": "c", "accesskey": "c", "text": "It does not matter" } ], "messages": { "correct": "Correct. It does not matter because it is all the same quiz.", "incorrect": "No! It does not matter because it is all the same quiz." } }, { "id": "13", "question_visual_id": "2", "question_text": "The song _Is That All There Is_ was originally released in…", "answer_ids": [ "131" ], "answers": [ { "id": "131", "numerator": "a", "accesskey": "a", "text": "1967" }, { "id": "132", "numerator": "b", "accesskey": "b", "text": "1970" }, { "id": "133", "numerator": "c", "accesskey": "c", "text": "1901" }, { "id": "134", "numerator": "d", "accesskey": "d", "text": "Multiple times", "correction": "Wait, how could you pick that!? Did you read the question? *Originally* means only once silly." } ], "messages": { "correct": "Correct. While made famous by Peggy Lee in 1969, it was originally performed by Georgia Brown in May 1967 for a television special.", "incorrect": "Incorrect. While made famous by Peggy Lee in 1969, it was originally performed by Georgia Brown in May 1967 for a television special." } }, { "id": "14", "question_visual_id": "3", "question_text": "The song was made popular by Peggy Lee in 1969. What song was a hit for her before this one?", "answer_ids": [ "143" ], "answers": [ { "id": "141", "numerator": "a", "accesskey": "a", "text": "_The Devil Went Down to Georgia_" }, { "id": "142", "numerator": "b", "accesskey": "b", "text": "_Georgia On My Mind_", "correction": "No, that was [Georgia Brown](https://en.wikipedia.org/wiki/Georgia_Brown_(English_singer))" }, { "id": "143", "numerator": "c", "accesskey": "c", "text": "_Fever_" }, { "id": "144", "numerator": "d", "accesskey": "d", "text": "_A Mild Winter Cough_" } ], "messages": { "correct": "Correct. _Fever_ was her previous hit.", "incorrect": "Incorrect. _Fever_ was her previous hit." } }, { "id": "15", "question_visual_id": "4", "question_text": "Multiple people have covered the song. Which of these artists did *not* sing it?", "answer_ids": [ "153" ], "answers": [ { "id": "151", "numerator": "a", "accesskey": "a", "text": "Chaka Khan", "correction": "She covered it: [Chaka Khan](https://en.wikipedia.org/wiki/Chaka_Khan)" }, { "id": "152", "numerator": "b", "accesskey": "b", "text": "P.J. Harvey", "correction": "She covered it: [P.J. Harvey](https://en.wikipedia.org/wiki/P.J._Harvey)" }, { "id": "153", "numerator": "c", "accesskey": "c", "text": "Prince", "correction": "No, but he did record _Let\x27s go crazy_ which is arguably better." }, { "id": "154", "numerator": "d", "accesskey": "d", "text": "Homer Simpson", "correction": "In the _Children of a Lesser Clod_ episode of the Simpsons, Homer sings a verse of the chorus when asked by Rod Flanders to &quot;...sing that crazy song we love&quot;." }, { "id": "155", "numerator": "e", "accesskey": "e", "text": "Hildegard Knef", "correction": "Released a German version called _Wenn das alles ist_. [Hildegard Knef](https://en.wikipedia.org/wiki/Hildegard_Knef)" } ], "messages": { "correct": "Correct. Prince never covered it (although there\x27s a whole vault of content that\x27s never been released...)", "incorrect": "Incorrect. Prince was the one who never covered it." } }, { "id": "16", "question_visual_id": "5", "question_text": "Could you sing _Fever_ better than Rufferella?", "answer_ids": [ "162" ], "answers": [ { "id": "161", "numerator": "y", "accesskey": "y", "text": "Yes" }, { "id": "162", "numerator": "n", "accesskey": "n", "text": "No" } ], "messages": { "correct": "Correct. Correct, she sang for the queen, did you? [Rufferella](https://amzn.com/0747550387?)", "incorrect": "Incorrect, she sang for the queen. Did you? No, I didn\x27t think so. [Rufferella](https://amzn.com/0747550387?)" } }, { "id": "17", "question_visual_id": "6", "question_text": "The lyrics talk of being disappointed with various experiences in her life. What experience did *not* happen to the protaganist?", "answer_ids": [ "174" ], "answers": [ { "id": "171", "numerator": "a", "accesskey": "a", "text": "Her family\x27s house caught on fire" }, { "id": "172", "numerator": "b", "accesskey": "b", "text": "She saw the circus" }, { "id": "173", "numerator": "c", "accesskey": "c", "text": "Falling in love" }, { "id": "174", "numerator": "d", "accesskey": "d", "text": "Her dog died" } ], "messages": { "correct": "Correct. Her dog was not mentioned.", "incorrect": "Incorrect. Nothing happened to her dog." } }, { "id": "18", "question_visual_id": "5", "question_text": "Is that all there is?", "answer_ids": [ "182", "183" ], "answers": [ { "id": "181", "numerator": "y", "accesskey": "y", "text": "Yes" }, { "id": "182", "numerator": "n", "accesskey": "n", "text": "No" } ], "messages": { "correct": "Trick question. Both answers are correct. So let\x27s &quot;break out the booze and have a ball — if that\x27s all there is.&quot;", "incorrect": "Trick question. Both answers are correct. So let\x27s &quot;break out the booze and have a ball — if that\x27s all there is.&quot;" } } ] }'
+var q1Data = '{ "introduction": "This quiz is about the song _Is That All There Is?_", "final_text": "Read more on Wikipedia\x27s entry on [Is that all there is?](https://en.wikipedia.org/wiki/Is_That_All_There_Is%3F)", "questions": [ { "id": "12", "question_visual_id": "1", "question_text": "Select a quiz type.", "answer_ids": [ "123" ], "answers": [ { "id": "121", "numerator": "a", "accesskey": "a", "text": "Hard quiz" }, { "id": "122", "numerator": "b", "accesskey": "b", "text": "Easy quiz" }, { "id": "123", "numerator": "c", "accesskey": "c", "text": "It does not matter" } ], "messages": { "correct": "Correct. It does not matter because it is all the same quiz.", "incorrect": "No! It does not matter because it is all the same quiz." } }, { "id": "13", "question_visual_id": "2", "question_text": "The song _Is That All There Is_ was originally released in…", "answer_ids": [ "131" ], "answers": [ { "id": "131", "numerator": "a", "accesskey": "a", "text": "1967" }, { "id": "132", "numerator": "b", "accesskey": "b", "text": "1970" }, { "id": "133", "numerator": "c", "accesskey": "c", "text": "1901" }, { "id": "134", "numerator": "d", "accesskey": "d", "text": "Multiple times", "correction": "*Originally* means only once silly." } ], "messages": { "correct": "Correct. While made famous by Peggy Lee in 1969, it was originally performed by Georgia Brown in May 1967 for a television special.", "incorrect": "Incorrect. While made famous by Peggy Lee in 1969, it was originally performed by Georgia Brown in May 1967 for a television special." } }, { "id": "14", "question_visual_id": "3", "question_text": "The song was made popular by Peggy Lee in 1969. What song was a hit for her before this one?", "answer_ids": [ "143" ], "answers": [ { "id": "141", "numerator": "a", "accesskey": "a", "text": "_The Devil Went Down to Georgia_" }, { "id": "142", "numerator": "b", "accesskey": "b", "text": "_Georgia On My Mind_", "correction": "No, that was [Georgia Brown](https://en.wikipedia.org/wiki/Georgia_Brown_(English_singer))" }, { "id": "143", "numerator": "c", "accesskey": "c", "text": "_Fever_" }, { "id": "144", "numerator": "d", "accesskey": "d", "text": "_A Mild Winter Cough_" } ], "messages": { "correct": "Correct. _Fever_ was her previous hit.", "incorrect": "Incorrect. _Fever_ was her previous hit." } }, { "id": "15", "question_visual_id": "4", "question_text": "Multiple people have covered the song. Which of these artists did *not* sing it?", "answer_ids": [ "153" ], "answers": [ { "id": "151", "numerator": "a", "accesskey": "a", "text": "Chaka Khan", "correction": "She covered it: [Chaka Khan](https://en.wikipedia.org/wiki/Chaka_Khan)" }, { "id": "152", "numerator": "b", "accesskey": "b", "text": "P.J. Harvey", "correction": "She covered it: [P.J. Harvey](https://en.wikipedia.org/wiki/P.J._Harvey)" }, { "id": "153", "numerator": "c", "accesskey": "c", "text": "Prince", "correction": "Correct. However, he did record _Let\x27s go crazy_ which is arguably better." }, { "id": "154", "numerator": "d", "accesskey": "d", "text": "Homer Simpson", "correction": "In the _Children of a Lesser Clod_ episode of the Simpsons, Homer sings a verse of the chorus when asked by Rod Flanders to &quot;...sing that crazy song we love&quot;." }, { "id": "155", "numerator": "e", "accesskey": "e", "text": "Hildegard Knef", "correction": "Released a German version called _Wenn das alles ist_. [Hildegard Knef](https://en.wikipedia.org/wiki/Hildegard_Knef)" } ], "messages": { "correct": "Correct. Prince never covered it (although there\x27s a whole vault of content that\x27s never been released...)", "incorrect": "Incorrect. Prince was the one who never covered it." } }, { "id": "16", "question_visual_id": "5", "question_text": "Could you sing _Fever_ better than Rufferella?", "answer_ids": [ "162" ], "answers": [ { "id": "161", "numerator": "y", "accesskey": "y", "text": "Yes" }, { "id": "162", "numerator": "n", "accesskey": "n", "text": "No" } ], "messages": { "correct": "Correct. She sang for the queen, did you? [Rufferella](https://amzn.com/0747550387?)", "incorrect": "Incorrect, she sang for the queen. Did you? No, I didn\x27t think so. [Rufferella](https://amzn.com/0747550387?)" } }, { "id": "17", "question_visual_id": "6", "question_text": "The lyrics talk of being disappointed with various experiences in her life. What experience did *not* happen to the protaganist?", "answer_ids": [ "174" ], "answers": [ { "id": "171", "numerator": "a", "accesskey": "a", "text": "Her family\x27s house caught on fire" }, { "id": "172", "numerator": "b", "accesskey": "b", "text": "She saw the circus" }, { "id": "173", "numerator": "c", "accesskey": "c", "text": "Falling in love" }, { "id": "174", "numerator": "d", "accesskey": "d", "text": "Her dog died" } ], "messages": { "correct": "Correct. Her dog was not mentioned.", "incorrect": "Incorrect. Nothing happened to her dog." } }, { "id": "18", "question_visual_id": "5", "question_text": "Is that all there is?", "answer_ids": [ "182", "183" ], "answers": [ { "id": "181", "numerator": "y", "accesskey": "y", "text": "Yes" }, { "id": "182", "numerator": "n", "accesskey": "n", "text": "No" } ], "messages": { "correct": "Trick question. Both answers are correct. So let\x27s &quot;break out the booze and have a ball — if that\x27s all there is.&quot;", "incorrect": "Trick question. Both answers are correct. So let\x27s &quot;break out the booze and have a ball — if that\x27s all there is.&quot;" } } ] }'
 
 
 var quiz_controller;
@@ -351,9 +339,8 @@ $(document).ready(function() {
 	$("#quiz1link").click(function(event) {
 		event.preventDefault();
 	  	loadQuestion1(); 
+	  	quiz_controller.run();
 	})
-	loadQuestion1();
-	quiz_controller.run();
 });
 
 function loadQuestion1() {
@@ -369,6 +356,11 @@ function hide_introduction() {
 	intro.addClass('hidden');
 }
 
+function show_introduction() {
+	var intro = $("#introduction");
+	intro.removeAttr('hidden');
+	intro.removeClass('hidden');
+}
 function loadQuestion(qdata) {
 	var jsonObject = JSON.parse(qdata);
 	var qc = new QuizController(jsonObject);
